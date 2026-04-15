cae7e8cc7bcc041c343e59838601fc63618bb0bc
Cuba Giesbrecht <banishlight@gmail.com>
2026-04-14 21:33:53 -0600

    Basic Music View

--- inc/views/MusicView.hpp
+++ inc/views/MusicView.hpp
@@ -14,4 +14,10 @@ class MusicView : public View {
     private:
         void _fetchEvents();
         std::vector<std::unique_ptr<Widget>> _widgets;
+        std::string _title      = "No track";
+        std::string _artist     = "No artist";
+        std::string _deviceName = "Not connected";
+        bool        _connected  = false;
+        Font _font;
+        const std::string _fontPath = "assets/roboto.ttf";
 };
--- src/views/MusicView.cpp
+++ src/views/MusicView.cpp
@@ -13,7 +13,24 @@ void MusicView::close() {
 }
 
 void MusicView::draw() {
+    const int W = 1024;
+    const int H = 600;
 
+    DrawRectangle(0, 0, W, H, {10, 10, 10, 255});
+
+    // Connection status
+    Color statusColor = _connected ? GREEN : RED;
+    DrawTextEx(_font, _connected ? "Connected" : "Disconnected",
+               {20, 20}, 20, 1, statusColor);
+    DrawTextEx(_font, _deviceName.c_str(), {20, 46}, 16, 1, GRAY);
+
+    // Track info
+    DrawTextEx(_font, _title.c_str(),  {20, 300}, 48, 1, WHITE);
+    DrawTextEx(_font, _artist.c_str(), {20, 360}, 28, 1, GRAY);
+
+    // Back button
+    DrawRectangle(8, H - 52, 160, 36, {60, 60, 60, 255});
+    DrawTextEx(_font, "BACK", {28, (float)(H - 40)}, 16, 1, WHITE);
 }
 
 int MusicView::logic() {
@@ -23,5 +40,41 @@ int MusicView::logic() {
 }
 
 void MusicView::_fetchEvents() {
-
+    auto events = ViewHandler::getInstance().popViewEvents();
+    for (auto& e : events) {
+        switch (e->type) {
+            case ViewEvent::Type::BLUETOOTH: {
+                auto* bt = static_cast<BTEvent*>(e.get());
+                switch (bt->btType) {
+                    case BTEvent::BTType::DeviceConnected:
+                        _connected  = true;
+                        _deviceName = bt->deviceName;
+                        break;
+                    case BTEvent::BTType::DeviceDisconnected:
+                        _connected  = false;
+                        _title      = "No track";
+                        _artist     = "No artist";
+                        break;
+                    case BTEvent::BTType::TrackChanged:
+                        _title  = bt->title;
+                        _artist = bt->artist;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            }
+            case ViewEvent::Type::INPUT: {
+                auto* input = static_cast<InputEvent*>(e.get());
+                if (input->inputType == InputEvent::InputType::TAP) {
+                    for (auto& w : _widgets) {
+                        if (w->handleEvent(*input)) break;
+                    }
+                }
+                break;
+            }
+            default:
+                break;
+        }
+    }
 }
