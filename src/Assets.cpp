f0c26d367e3c039c795ce4eefece519dc5d418b3
Cuba Giesbrecht <banishlight@gmail.com>
2026-04-16 04:36:56 -0600

    Bad font file name

--- src/Assets.cpp
+++ src/Assets.cpp
@@ -12,9 +12,9 @@ namespace Assets {
     void load() {
         mainFont = LoadFontEx("assets/roboto.ttf", 64, nullptr, 0);
         catFont = LoadFontEx("assets/Rajdhani-Regular.ttf", 64, nullptr, 0);
-        catFont16 = LoadFontEx("assets/fonts/rajdhani.ttf", 16, nullptr, 0);
-        catFont24 = LoadFontEx("assets/fonts/rajdhani.ttf", 24, nullptr, 0);
-        catFont52 = LoadFontEx("assets/fonts/rajdhani.ttf", 52, nullptr, 0);
+        catFont16 = LoadFontEx("assets/Rajdhani-Regular.ttf", 16, nullptr, 0);
+        catFont24 = LoadFontEx("assets/Rajdhani-Regular.ttf", 24, nullptr, 0);
+        catFont52 = LoadFontEx("assets/Rajdhani-Regular.ttf", 52, nullptr, 0);
         gearIcon  = LoadTexture("assets/gear.png");
     }
 
