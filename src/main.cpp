08fd443c322128aeac6f6deeaae1b2c222a16d7b
Cuba Giesbrecht <banishlight@gmail.com>
2026-08-12 23:55:00 +0100

    GPS now running

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,24 @@
 #include <agents/OBDAgent.hpp>
 #include <agents/BluetoothAgent.hpp>
 #include <agents/SystemAgent.hpp>
+#include <agents/GPSAgent.hpp>
 
 int main(void) {
     OBDAgent obda;
     BluetoothAgent bta;
     SystemAgent sa;
+    GPSAgent gpsa;
     DisplayAgent ga;
 
     obda.start();
     bta.start();
     sa.start();
+    gpsa.start();
     ga.start(); // This agent runs on current main thread
-    
+
     obda.stop();
     bta.stop();
     sa.stop();
+    gpsa.stop();
     return 0;
 }
